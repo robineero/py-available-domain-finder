422f9d460f06f5004d55d63a1934949753db102c
rolaur <rolaur@ttu.ee>
2020-08-19 01:24:04 +0300

    First version of available domain finder.

---
+++ domain-finder.py
@@ -0,0 +1,20 @@
+import whois
+from itertools import product
+
+c = 1
+file1 = open("domains-available.txt", "w")
+alphabet = "abcdefghijklmnopqrstuvwxyz"  # Letters you would like to use.
+domain_length = 4
+for x in product(alphabet, repeat=domain_length):
+    pref = "".join(x)  # Separate pref variable to set additional conditions for domain.
+    domain = pref + ".ee"
+    c += 1
+
+    condition = (pref[0] == pref [3]) and (pref[1] == pref [2])
+    if c % 1000 == 0:
+        print(c)  # Shows that program works in console.
+    if condition and whois.whois(domain).status is None:
+        file1.write(domain + "\n")
+
+file1.close()
+print("Done!")
